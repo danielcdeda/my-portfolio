edae23fff3e20f511d9577d996b008088fc12c97
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-09-01 17:53:54 -0300

    started banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Navbar/Header';
+import Banner from './components/Banner/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
     
   );
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,28 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+
+function Banner() {
+    return (
+        <section className="banner" id="home">
+            <Container className="align-items-center">
+                <Row>
+                    <Col>
+
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Banner
