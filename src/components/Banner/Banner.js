cfb3ee9f666c14251dcecccbdc252bd68fbb3464
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-09-01 18:43:22 -0300

    updated banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,13 +1,28 @@
 import { Container, Row, Col } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import BannerImg from '../../assets/img/menino-note-final.png'
+import { useState } from "react"
 
 
 function Banner() {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false); 
+
+
+    const toRotate = ["Desenvolvedor Front-end", "UI/UX Designer"]
+
     return (
         <section className="banner" id="home">
             <Container className="align-items-center">
                 <Row>
-                    <Col>
-
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Bem vindo ao meu site</span>
+                        <h1>{'Olá, eu sou um desenvolvedor Front-End!'}<span>UI/UX Design</span></h1>
+                        <p></p>
+                        <button onClick={() => console.log('connect')}>Fale comigo! <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col cs={12} md={6} xl={5}>
+                        <img src={BannerImg} alt="Banner-img" />
                     </Col>
                 </Row>
             </Container>
