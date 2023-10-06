f0f0ea8a1bef2b8cf204755a8756ad5dda1ebbaa
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-10-06 16:26:37 -0300

    Changed skills content

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from '../../assets/img/meter1.png'
-import meter2 from '../../assets/img/meter2.png'
-import meter3 from '../../assets/img/meter3.png'
+import jslogo from '../../assets/img/jslogo.png'
+import nodejslogo from '../../assets/img/nodejslogo.png'
+import reactlogo from '../../assets/img/reactlogo.png'
+import csslogo from '../../assets/img/csslogo.png'
+import htmllogo from '../../assets/img/htmllogo.png'
 import './skills.css'
 import gradient from '../../assets/img/Gradient.png'
 
@@ -38,23 +40,23 @@ function Skills () {
                         <p>Algumas das ferramentas que aprendi a utilizar durante minha jornada como desenvolvedor web.</p>
                         <Carousel responsive={responsive} infinite={true} className="skill-slider custom-carousel-transition"> 
                             <div className="item">
-                                <img src={meter3} alt="Image" className="imageMeter" /> 
+                                <img src={reactlogo} alt="Image" className="imageMeter" /> 
                                 <h5 className="Stacks">React</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" className="imageMeter" /> 
+                                <img src={jslogo} alt="Image" className="imageMeter" /> 
                                 <h5 className="Stacks">Javascript</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" className="imageMeter" /> 
+                                <img src={nodejslogo} alt="Image" className="imageMeter" /> 
                                 <h5 className="Stacks">NodeJS</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image"  className="imageMeter" /> 
+                                <img src={csslogo} alt="Image"  className="imageMeter" /> 
                                 <h5 className="Stacks">CSS3</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" className="imageMeter"/> 
+                                <img src={htmllogo} alt="Image" className="imageMeter"/> 
                                 <h5 className="Stacks">HTML5</h5>
                             </div>
                         </Carousel>
